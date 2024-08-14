1bd45e00991352adfa10484ff93ae5d7ef8f4cf6
dengbowen666 <2763201730@qq.com>
2024-08-14 18:43:26 +0800

    Push Files: 2024/08/14, 18:43:26

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {resolve}  from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-   base: './',
+  base:'/Vue3project-guiguzhenxuan/',
   resolve: {
   // 设置文件./src路径为 @
    alias: {
