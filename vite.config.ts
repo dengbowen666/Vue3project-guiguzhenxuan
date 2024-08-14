4519d920a5413db94827f8213d359c39e025217a
dengbowen666 <2763201730@qq.com>
2024-08-14 22:25:10 +0800

    Push Files: 2024/08/14, 22:25:10

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import { viteMockServe } from "vite-plugin-mock";
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command }: ConfigEnv) => {
   return {
     plugins: [
       vue(),
